b247164b1f746c007b061395501bde4e3acad712
Fanny Rostaing <fanny@coffretis.com>
2020-09-14 17:13:03 +0200

    routes ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in?
+      redirect_to wall_path
+    end
+  end
+
+  def wall
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get '/wall', to: 'pages#wall', as: 'wall'
+
+  resources :circles, only: [ :index, :show ] do
+    resources :recommendations, only: [ :show ]
+    resources :movies, only: [:index, :show]
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
