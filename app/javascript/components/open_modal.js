f860eb2b48ea63650a3d98f65585cd3a8043507f
Fanny Rostaing <fanny@coffretis.com>
2020-09-23 11:26:21 +0200

    refacto

---
+++ app/javascript/components/open_modal.js
@@ -0,0 +1,18 @@
+const openModal = () => {
+
+  const button = document.querySelector('#open-modal');
+
+  if (button === undefined)
+    return
+  console.log('je suis dans openModal');
+
+  button.addEventListener('click', (event) => {
+    console.log('je suis dans event listener');
+    const form = document.querySelector('#modal-form');
+    form.classList.toggle('hidden');
+
+  })
+};
+
+
+  export { openModal }
