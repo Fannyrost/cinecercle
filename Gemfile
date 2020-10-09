5979d8a5f4f609348a4625e42619b6294586174a
Fanny Rostaing <fanny@coffretis.com>
2020-10-09 15:16:33 +0200

    add cloudinary

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'dotenv-rails', groups: [:development, :test]
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
+
+gem 'cloudinary', '~> 1.16.0'
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,19 @@ class User < ApplicationRecord
   has_many :memberships
   has_many :circles, through: :memberships
   has_many :watchlists
+  has_one_attached :photo
 
   def reco_count
     reco_count = 0
     self.memberships.each { |m| reco_count +=1 }
     reco_count
+  end
+
+  def profile_picture
+    if photo.attached?
+      photo.key
+    else
+      "avatar-unknown.png"
+    end
   end
 end
