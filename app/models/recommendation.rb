2a21d14285edfa06d5ad800c265fc7efe86a73a6
Fanny Rostaing <fanny@coffretis.com>
2020-10-16 12:42:33 +0200

    small debug

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -9,9 +9,9 @@ class Recommendation < ApplicationRecord
     time_ago = TimeDifference.between(timestamp, now).in_seconds
     case time_ago
     when 0..119        then "à l'instant"
-    when 120..3599     then "il y a #{delta / 60} minutes"
-    when 3600..86399   then "il y a #{(delta / 3600).round} heures"
-    when 86400..604800 then "il y a #{(delta / 86400).round} jours"
+    when 120..3599     then "il y a #{time_ago / 60} minutes"
+    when 3600..86399   then "il y a #{(time_ago / 3600).round} heures"
+    when 86400..604800 then "il y a #{(time_ago / 86400).round} jours"
     else "il y a #{(TimeDifference.between(timestamp, now).in_weeks).round} semaines"
     end
 
